69aee299ee869af52b267f8d3e15bfca669a7987
Febri <root.febri@gmail.com>
2024-08-05 22:54:05 +0700

    v0.2

--- app/Console/Commands/Indexing.php
+++ app/Console/Commands/Indexing.php
@@ -14,19 +14,27 @@ use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Console\Command;
 use Throwable;
 
-class indexing extends Command
+class Indexing extends Command
 {
     use GoogleOAuth, HasConstant;
 
     protected const ENDPOINT_URL_UPDATED = 'https://indexing.googleapis.com/v3/urlNotifications:publish';
     public int $submitted = 0;
+
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
     protected $signature = 'indexing';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
     protected $description = 'Start indexing process';
+
     protected array $urlLists = [];
     protected ?string $sitemap = '';
     protected ServiceAccount $account;
@@ -38,6 +46,7 @@ class indexing extends Command
     {
         if (!$this->extractUrls()) return;
         if (!$this->confirm('Continue indexing?', true)) return;
+
         $this->line("Indexing started...");
         $this->cleanup();
         $this->selectServiceAccount();
@@ -47,10 +56,8 @@ class indexing extends Command
             $gVerify = "{$this->account->google_verifcation}.html";
             $linkVerify = str_replace($baseFile, $gVerify, $this->sitemap);
             $areYouOwner = @file_get_contents($linkVerify);
-            if (!$areYouOwner) {
-                if (!$this->confirm("$linkVerify tidak ditemukan, Tetap lanjutkan?")) {
-                    return;
-                }
+            if (!$areYouOwner && !$this->confirm("$linkVerify not found, continue anyway?")) {
+                return;
             }
         }
 
@@ -63,7 +70,7 @@ class indexing extends Command
         while (!str_starts_with($this->sitemap, 'https://')) {
             $this->sitemap = $this->ask("Enter URLs sitemap to be indexed");
             if (strlen($this->sitemap) < 1) {
-                $this->line("Invalid url, eg: https://example.com/sitemap.xml");
+                $this->line("Invalid URL, e.g., https://example.com/sitemap.xml");
             }
         }
 
@@ -89,11 +96,17 @@ class indexing extends Command
     {
         $this->line("Cleaning up indexed URLs...");
 
-        $lessThan24Hours = Indexed::where('success', true)->where('created_at', '>', now()->subHours(24))->where('sitemap_url', $this->sitemap)->pluck('url')->toArray();
+        $skipThis = Indexed::all()
+            ->where('sitemap_url', $this->sitemap)
+            ->where('success', true)
+            ->filter(fn($res) => -24 < $res->last_update)
+            ->pluck('url')
+            ->toArray();
 
-        $this->urlLists = array_filter($this->urlLists, fn($url) => !in_array($url, $lessThan24Hours));
-        $this->line(count($this->urlLists) . " URLs left after cleanup");
-        $this->confirm('Randomize urls?', true) && shuffle($this->urlLists);
+        $from = count($this->urlLists);
+        $this->urlLists = array_diff($this->urlLists, $skipThis);
+        $this->line($from - count($this->urlLists) . " URLs removed after cleanup");
+        $this->confirm('Randomize URLs?', true) && shuffle($this->urlLists);
     }
 
     protected function selectServiceAccount(): void
@@ -101,12 +114,7 @@ class indexing extends Command
         $accounts = ServiceAccount::all()->load('oauths');
         $fmt = array_map(function ($email) {
             $limits = ServiceAccount::where('email', $email)?->first()->oauths()->pluck('limit')->toArray();
-
-            $oauthLimit = 0;
-            foreach ($limits as $limit) {
-                $oauthLimit += $limit ?? 0;
-            }
-
+            $oauthLimit = array_sum($limits);
             return "$email => [limit: $oauthLimit]";
         }, $accounts->pluck('email')->toArray());
 
@@ -158,14 +166,14 @@ class indexing extends Command
                 $oauth->decrement('limit');
             }
 
-            $status === false && $this->alwaysContinue && $this->alwaysContinue = $this->confirm('Request gagal, selalu tanya untuk melanjutkan?', true);
+            $status === false && $this->alwaysContinue && $this->alwaysContinue = $this->confirm('Request failed, always ask to continue?', true);
             if (!$oauth->usable()) {
                 return;
             }
         }
     }
 
-    private function tryAuth(OAuthModel $oauth, $retry = 3): Client|ClientInterface|null
+    private function tryAuth(OAuthModel $oauth, int $retry = 3): Client|ClientInterface|null
     {
         $sleep = 5;
         try {
@@ -174,7 +182,7 @@ class indexing extends Command
         } catch (Throwable) {
             if ($retry > 0) {
                 sleep($sleep);
-                return $this->tryAuth($retry - 1);
+                return $this->tryAuth($oauth, $retry - 1);
             } else {
                 return null;
             }
@@ -191,25 +199,31 @@ class indexing extends Command
         try {
             $response = @$request->post(self::ENDPOINT_URL_UPDATED, ['body' => json_encode($this->content)]);
             $code += $response->getStatusCode();
-        } catch (GuzzleException $e) {
-            if ($hasMessage = @json_decode($e->getMessage())) {
-                $this->line("LINE: $this->submitted | Message/Code: " . $hasMessage['error']['message'] ?? $code);
-            } else {
-                $this->logError("LINE: $this->submitted | Error occurred while indexing URL: $url", $e);
-            }
-
-            $indexed->success = false;
-            $indexed->save();
-            $this->newLine();
-            return null;
-        } catch (Throwable $e) {
-            $this->logError("LINE: $this->submitted | Error occurred while indexing URL: $url", $e->getCode());
-            $indexed->success = false;
-            $indexed->save();
-            $this->newLine();
+        } catch (GuzzleException|Throwable $e) {
+            $this->handleException($e, $indexed, $url);
             return null;
         }
 
+        $this->updateIndexedStatus($indexed, $code, $url);
+        return $code >= 200 && $code < 300 ? true : ($code >= 300 && $code < 600 ? false : null);
+    }
+
+    protected function handleException(Throwable $e, Indexed $indexed, string $url): void
+    {
+        $this->logError("LINE: $this->submitted | Error occurred while indexing URL: $url", $e->getCode());
+        $indexed->success = false;
+        $indexed->save();
+        $this->newLine();
+    }
+
+    protected function logError(string $message, int $code): void
+    {
+        $this->info($message);
+        $this->info("Error code: $code");
+    }
+
+    protected function updateIndexedStatus(Indexed $indexed, int $code, string $url): void
+    {
         if ($code >= 200 && $code < 300) {
             $indexed->success = true;
             $this->line("LINE: $this->submitted | [$code] $url -> Request success!");
@@ -219,12 +233,5 @@ class indexing extends Command
         }
         $indexed->save();
         $this->newLine();
-        return $code >= 200 && $code < 300 ? true : ($code >= 300 && $code < 600 ? false : null);
-    }
-
-    protected function logError(string $message, Throwable $e): void
-    {
-        $this->info($message);
-        $this->info($e->getMessage());
     }
 }
--- app/Models/Indexed.php
+++ app/Models/Indexed.php
@@ -2,11 +2,31 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 class Indexed extends Model
 {
     protected $table = 'indexeds';
     protected $fillable = ['sitemap_url', 'url', 'success'];
+
+    protected $casts = [
+        'success' => 'boolean',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'success' => 'boolean',
+        ];
+    }
+
+    protected $appends = ['last_update'];
+
+    public function getLastUpdateAttribute(): float
+    {
+        return Carbon::now()->diffInHours($this->updated_at);
+    }
 }
