d6d2407708aadaa3367f80b1f1b79dcb125255c5
Febri <root.febri@gmail.com>
2024-08-07 19:33:35 +0700

    ADDED MAX LOOP SAFETY

--- app/Traits/HasArcS3.php
+++ app/Traits/HasArcS3.php
@@ -3,13 +3,29 @@
 
 namespace App\Traits;
 
-use App\Types\S3ArcType;
 use Illuminate\Support\Facades\Cache;
 use Throwable;
 
 trait HasArcS3
 {
     private const CACHE_ARC_LOCK_TIMEOUT_SEC = 3;
+
+    public function create($data): void
+    {
+        Cache::forever(self::CACHE_WORKER_KEY, $data);
+    }
+
+    public function add(array $data): void
+    {
+        $this->lock();
+
+        $params = $this->read();
+        $params[] = $data;
+        Cache::forever(self::CACHE_WORKER_KEY, $params);
+
+        $this->unlock();
+    }
+
     private function lock(): void
     {
         $key = self::CACHE_WORKER_KEY . ".lock";
@@ -22,32 +38,16 @@ trait HasArcS3
         $_ = Cache::put($key, true, $this::CACHE_ARC_LOCK_TIMEOUT_SEC);
     }
 
-    private function unlock(): void
-    {
-        $key = self::CACHE_WORKER_KEY . ".lock";
-        $_ = Cache::pull($key);
-    }
-
-    public function create($data): void
-    {
-        Cache::forever(self::CACHE_WORKER_KEY, $data);
-    }
-
     public function read(bool $count = false): mixed
     {
         $read = Cache::get(self::CACHE_WORKER_KEY) ?? [];
         return $count ? count($read) : $read;
     }
 
-    public function add(array $data): void
+    private function unlock(): void
     {
-        $this->lock();
-
-        $params = $this->read();
-        $params[] = $data;
-        Cache::forever(self::CACHE_WORKER_KEY, $params);
-
-        $this->unlock();
+        $key = self::CACHE_WORKER_KEY . ".lock";
+        $_ = Cache::pull($key);
     }
 
     public function pullFirst(): array
